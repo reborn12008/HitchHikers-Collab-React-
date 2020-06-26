1f9a220b665020867209ef28e31544859e2f7aee
Filipe <reborn12008@gmail.com>
2020-06-26 04:38:40 +0100

    Event Info page on list click

--- src/components/eventSearchers.jsx
+++ src/components/eventSearchers.jsx
@@ -22,7 +22,9 @@ class SearchPageContent extends React.Component{
 				<Route exact path="/festivals" component={SearchTypeToggler}/>
 				<Route exact path="/festivals/categorySearch" component={CategorySearch}/>
 				<Route exact path="/festivals/textSearch" component={TextSearch}/>
-				<Route exact path="/festivals/searchResults=" component={SearchResults}/>
+				<Route exact path="/festivals/textSearchResults=" component={TextSearchResults}/>
+				<Route exact path="/festivals/categorySearchResults=" component={CategorySearchResults}/>
+				<Route exact path="/juvafestival" component={EventInfo}/>
         	</Switch>        
 			
 		</>
@@ -74,35 +76,38 @@ class CategorySearch extends React.Component{
 	}
 	render() {
     return (
-		<div className="pt-5">	
-			<label htmlFor="startDate" className="col-sm-10 col-form-label col-form-label-lg">Procurar eventos entre</label><br/>
-				<div className="ml-4">
-					<input type="date" id="startDate" onChange={this.handleStartDateChange.bind(this)}/>
-					<label className="col-form-label col-form-label-lg" style={{ margin:"0px 10px" }}>e</label>
-					<input type="date" id="endDate" onChange={this.handleEndDateChange.bind(this)}/> <br/>
-				</div>
+		<>
+			<SearchTypeToggler/>
+			<div className="pt-5">	
+				<label htmlFor="startDate" className="col-sm-10 col-form-label col-form-label-lg">Procurar eventos entre</label><br/>
+					<div className="ml-4">
+						<input type="date" id="startDate" onChange={this.handleStartDateChange.bind(this)}/>
+						<label className="col-form-label col-form-label-lg" style={{ margin:"0px 10px" }}>e</label>
+						<input type="date" id="endDate" onChange={this.handleEndDateChange.bind(this)}/> <br/>
+					</div>
 
-			<label htmlFor="type-select" className="col-form-label col-form-label-lg" style={{ margin:"0px 10px" }}>Selecione um género:</label>
-			<select className="custom-select custom-select-lg mb-3 ml-4" id="type-select" onChange={this.handleCategoryChange.bind(this)}>
-				<option value="select">...</option>
-				{ Object.keys(categories).map((key, index) =>( <option value={key}> {categories[key]} </option>))}
-			</select><br/>
+				<label htmlFor="type-select" className="col-form-label col-form-label-lg" style={{ margin:"0px 10px" }}>Selecione um género:</label>
+				<select className="custom-select custom-select-lg mb-3 ml-4" id="type-select" onChange={this.handleCategoryChange.bind(this)}>
+					<option value="select">...</option>
+					{ Object.keys(categories).map((key, index) =>( <option value={key}> {categories[key]} </option>))}
+				</select><br/>
 
-			<label htmlFor="country-select" className="col-form-label col-form-label-lg" style={{ margin:"0px 10px" }}>Selecione o país onde decorrerá o evento</label>
-			<select className="custom-select custom-select-lg mb-3 ml-4" id="country-select" onChange={this.handleCountryChange.bind(this)}>
-				<option value="select">...</option>
-				{ this.state.countries.map(countries => <option>{countries.name}</option>)}
-			</select>
-			<Link className ="btn btn-info ml-3" id="textSearch" to={{ pathname: "/festivals/searchResults=", 
-																	state:{startDate : this.state.startDate,
-																		endDate: this.state.endDate,
-																		categorySelected : this.state.categorySelected,
-																		countrySelected : this.state.countrySelected,
-																		} }}>
-				Procurar
-			</Link><br/>
-		</div>
-    );
+				<label htmlFor="country-select" className="col-form-label col-form-label-lg" style={{ margin:"0px 10px" }}>Selecione o país onde decorrerá o evento</label>
+				<select className="custom-select custom-select-lg mb-3 ml-4" id="country-select" onChange={this.handleCountryChange.bind(this)}>
+					<option value="select">...</option>
+					{ this.state.countries.map(countries => <option>{countries.name}</option>)}
+				</select>
+				<Link className ="btn btn-info ml-3" id="categorySearch" to={{ pathname: "/festivals/categorySearchResults=", 
+																		state:{startDate : this.state.startDate,
+																			endDate: this.state.endDate,
+																			categorySelected : this.state.categorySelected,
+																			countrySelected : this.state.countrySelected,
+																			} }}>
+					Procurar
+				</Link><br/>
+			</div>
+		</>
+	);
 	}
 }
 
@@ -118,38 +123,25 @@ class TextSearch extends React.Component{
 
 	render() {
     return (
-		<div className="mt-3">
-			<br/><br/>
-		    <input id="text-search" type="text" onChange={this.handleSearchText.bind(this)} />
-			<Link className ="btn btn-info ml-3" id="textSearch" to={{ pathname: "/festivals/searchResults=", state:{textInput : this.state.textkeysearch} }}>
-				Procurar
-			</Link><br/>
-			<span>*Insira uma palavra-chave - pode ser o nome do evento, o sítio onde irá decorrer, entre outros...</span>
-			
-		</div>
+		<>
+			<SearchTypeToggler/>
+			<div className="mt-3">
+				<br/><br/>
+				<input id="text-search" type="text" onChange={this.handleSearchText.bind(this)} />
+				<Link className ="btn btn-info ml-3" id="textSearch" to={{ pathname: "/festivals/textSearchResults=", state:{textInput : this.state.textkeysearch} }}>
+					Procurar
+				</Link><br/>
+				<span>*Insira uma palavra-chave - pode ser o nome do evento, o sítio onde irá decorrer, entre outros...</span>
+				
+			</div>
+		</>
     );
 	}
 }
 
-class SearchResults extends React.Component{
+class TextSearchResults extends React.Component{
 	constructor(props){
 		super(props);
-
-		if(this.props.location.state.textInput != null){
-			this.setState({apiSettings: {params : {"q" : this.props.location.state.textInput}}})
-		}
-		if(this.props.location.state.startDate == null){
-			this.setState({apiSettings: {params : {"start.gte" : this.props.location.state.startDate}}})
-		}
-		if(this.props.location.state.endDate == null){
-			this.setState({apiSettings: {params : {"start.lte" : this.props.location.state.endDate}}})
-		}
-		if(this.props.location.state.startDate == null){
-			this.setState({apiSettings: {params : {"country" : this.props.location.state.countrySelected}}})
-		}
-		if(this.props.location.state.startDate == null){
-			this.setState({apiSettings: {params : {"category" : this.props.location.state.categorySelected}}})
-		}
 
 		this.state ={
 			logged : localStorage.getItem("logged"),
@@ -160,7 +152,7 @@ class SearchResults extends React.Component{
 					"Authorization" : "Bearer JK2DXXqhuW8FF-FHbPqlg3cdbVaqPLn8Siaies--",
 				},
 				params : {
-
+					"q" : this.props.location.state.textInput
 				}
 			},
 			searchResultsList : [],
@@ -175,8 +167,18 @@ class SearchResults extends React.Component{
 		{
 			return(
 				<ul>
-					{ this.state.searchResultsList.map(searchResultsList => <button class="list-group-item list-group-item-action">{searchResultsList.title}</button>)}
+					{ this.state.searchResultsList.map(searchResultsList => 
+					<Link className ="list-group-item list-group-item-action" id="textSearch" to={{ pathname: "/juvafestival", state:{
+																																	eventName : searchResultsList.title,
+																																	eventPlace : searchResultsList.location,
+																																	eventDate : searchResultsList.start
+																																	} }}>
+						{searchResultsList.title}
+					</Link>
+					)
+					}
 				</ul>
+				
 			);
 		}else{
 			return(
@@ -185,6 +187,86 @@ class SearchResults extends React.Component{
 				</ul>
 			);
 		}
+	}
+}
+
+class CategorySearchResults extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state ={
+			logged : localStorage.getItem("logged"),
+			requestURL : "https://api.predicthq.com/v1/events",
+			apiSettings:{
+				headers: {
+					"Accept" : "application/json",
+					"Authorization" : "Bearer JK2DXXqhuW8FF-FHbPqlg3cdbVaqPLn8Siaies--",
+				},
+				params : {
+					"start.gte" : this.props.location.state.startDate,
+					"start.lte" : this.props.location.state.endDate,
+					"country" : this.props.location.state.countrySelected,
+					"category" : this.props.location.state.categorySelected,
+				}
+			},
+			searchResultsList : [],
+		}
+	}
+	componentDidMount(){
+		axios.get(this.state.requestURL, this.state.apiSettings).then( res => { const searchResultsList = res.data; this.setState({ searchResultsList : searchResultsList.results })});
+
+	}
+		
+	render() {
+		if(this.state.logged == 1)
+		{
+			return(
+				<ul>
+					{ this.state.searchResultsList.map(searchResultsList => 
+					<Link className ="list-group-item list-group-item-action" id="textSearch" to={{ pathname: "/juvafestival", state:{
+																																	eventName : searchResultsList.title,
+																																	eventPlace : searchResultsList.location,
+																																	eventDate : searchResultsList.start
+																																	} }}>
+						{searchResultsList.title}
+					</Link>
+					)
+					}
+				</ul>
+				
+			);
+		}else{
+			return(
+				<ul>
+					{ this.state.searchResultsList.map(searchResultsList => <li class="list-group-item">{searchResultsList.title}</li>)}
+				</ul>
+			);
+		}
+	}
+}
+
+class EventInfo extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state = {
+			eventName : this.props.location.state.eventName,
+			eventPlace : this.props.location.state.eventPlace,
+			eventDate : this.props.location.state.eventDate,
+			
+		}
+	}
+	render(){
+		console.log("Nome:" + this.state.eventName + "\n" +
+			"Local:" + this.state.eventName + "\n" +
+			"Inicio do evento:" +  this.state.eventDate);
+		return(
+			<>
+			Nome: {this.state.eventName}
+			Local: {this.state.eventName},
+			Inicio do evento: {this.state.eventDate}
+			</>
+		);
 	}
 }
 
